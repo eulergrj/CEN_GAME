58d44bc3e59a4466ce9637964b7b4a214f1ac5f6
Euler Ribeiro <eulergrj@gmail.com>
2022-05-28 14:05:33 +0100

    api update

--- api/Inc/bootstrap.php
+++ api/Inc/bootstrap.php
@@ -3,7 +3,7 @@
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");
  
 // include main configuration file
-require_once PROJECT_ROOT_PATH . "/inc/config.php";
+require_once PROJECT_ROOT_PATH . "/Inc/config.php";
  
 // include the base controller file
 require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
