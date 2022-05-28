bd0eaa4af6cca00fb41b5cc11d2985071c367f8e
Euler Ribeiro <eulergrj@gmail.com>
2022-05-28 13:57:25 +0100

    api update

--- api/Inc/bootstrap.php
+++ api/Inc/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
-define("PROJECT_ROOT_PATH", __DIR__ . "/../");
+var_dump("PROJECT_ROOT_PATH", __DIR__ . "/../");
+die();
+// define("PROJECT_ROOT_PATH", __DIR__ . "/../");
  
-var_dump(PROJECT_ROOT_PATH);
 // // include main configuration file
 // require_once PROJECT_ROOT_PATH . "/inc/config.php";
  
