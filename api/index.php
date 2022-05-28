10d511bc579900c1caad95beeb1c6e9c375ffa88
Euler Ribeiro <eulergrj@gmail.com>
2022-05-28 13:56:19 +0100

    api update

--- api/index.php
+++ api/index.php
@@ -1,17 +1,17 @@
 <?php
 require __DIR__ . "/inc/bootstrap.php";
  
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
+// $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $uri = explode( '/', $uri );
  
-if ((isset($uri[3]) && $uri[3] != 'question') || !isset($uri[4])) {
-    header("HTTP/1.1 404 Not Found");
-    exit();
-}
+// if ((isset($uri[3]) && $uri[3] != 'question') || !isset($uri[4])) {
+//     header("HTTP/1.1 404 Not Found");
+//     exit();
+// }
  
-require PROJECT_ROOT_PATH . "/Controller/Api/QuestionController.php";
+// require PROJECT_ROOT_PATH . "/Controller/Api/QuestionController.php";
  
-$objFeedController = new QuestionController();
-$strMethodName = $uri[4] . 'Action';
-$objFeedController->{$strMethodName}();
+// $objFeedController = new QuestionController();
+// $strMethodName = $uri[4] . 'Action';
+// $objFeedController->{$strMethodName}();
 ?>
