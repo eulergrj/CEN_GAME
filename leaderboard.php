78c8a46c17e11a00a1f362e0ce18e7c21b0b5a71
Euler Ribeiro <eulergrj@gmail.com>
2022-05-29 17:26:23 +0100

    webpage update

---
+++ leaderboard.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+        <!-- Set the title bar of the page -->
+        <title>Classificação | Uma Aventura Cibersegura!</title>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    </head>
+
+    <body>
+        
+        <a href="#" id="menu-btn">
+            <img src="./assets/menu.png" alt="Menu" class="menu">
+        </a>
+        <div id="menu">
+            <div class="wrapper">
+                <a href="#" class="close-menu"><i class="fa fa-times"></i></a>
+                <img src="./assets/logo.png" alt="Uma Aventura Cibersegura" class="logo">
+                <ul class="menu-list">
+                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#howtoModal">Como Jogar</a></li>
+                    <li><a href="./leaderboard.html" target="_blank">Classificação</a></li>
+                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#creditModal">Créditos</a></li>                    
+                </ul>
+            </div>
+        </div>
+        
+
+        <script>
+            $(document).ready(function(){
+
+                $("#menu-btn").click(function(e){
+                    $("#menu").css("display", 'flex');
+                });
+
+                $(".close-menu").click(function(e){
+                    $("#menu").css("display", 'none');
+                });
+
+            });
+        </script>
+        
+    </body>
+</html>
